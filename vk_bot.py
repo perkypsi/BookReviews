149d93462934b33c9f668e10474f76e6b270a2f0
User <dfdf@fsdf.com>
2021-08-16 20:47:59 +0300

    Add system of logging and fix documentation.

--- vk_bot.py
+++ vk_bot.py
@@ -1,10 +1,16 @@
+# -*- coding: utf-8 -*-
+
 import csv
+import random
 from datetime import datetime
+import logging
+from settings import TOKEN
 
 import vk_api
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.longpoll import VkLongPoll, VkEventType
 
+"""Use Python 3.8"""
 
 class Vk_bot:
     def __init__(self, user_id):
@@ -73,13 +79,14 @@ class Vk_bot:
             writer = csv.writer(out_csv)
             writer.writerow(application_info)
         self.control_state_send["STATE_SEND"] = False
+        log.debug("Данные сформированы и записаны в файл")
         self.complete_application = True
 
     def write_msg(self, message, keyboard=None):
         post = {
             'user_id': self.user_id,
             'message': message,
-            'random_id': 0
+            'random_id': random.randint(0, 10000)
         }
         if keyboard is not None:
             post['keyboard'] = keyboard.get_keyboard()
@@ -92,14 +99,19 @@ class Vk_bot:
         if message == "оставить заявку":
             self.write_msg(message=self.types_message["CONTACT_DETAILS_MESSAGE"])
             self.control_state_send["STATE_CONTACT"] = True
+            log.info(f"{self.user_id} оставил заявку")
         elif self.control_state_send["STATE_CONTACT"]:
             self.get_contact(message=message)
+            log.info(f"{self.user_id} оставил контакты")
         elif self.control_state_send["STATE_GROUP"]:
             self.get_group(message=message)
+            log.info(f"{self.user_id} оставил данные группы")
         elif self.control_state_send["STATE_TEXT"]:
             self.get_content(message=message)
+            log.info(f"{self.user_id} оставил содержание заявки")
         elif self.control_state_send["STATE_FEEDBACK"]:
             self.get_feedback(message=message)
+            log.info(f"{self.user_id} оставил оценку")
             self.send_data()
         else:
             keyboard = VkKeyboard(one_time=True)
@@ -107,8 +119,20 @@ class Vk_bot:
             self.write_msg(message=self.types_message["GREETING_MESSAGE"], keyboard=keyboard)
 
 
+def log_configure(log_program):
+    file_handler = logging.FileHandler("bot.log", mode='w', encoding='utf8')
+    file_handler.setFormatter(logging.Formatter("%(asctime)ss| %(levelname)s | %(message)s"))
+    file_handler.setLevel(logging.DEBUG)
+    log_program.addHandler(file_handler)
+
+
+# Запуск средства логирования
+log = logging.getLogger("log_chat_bot")
+log_configure(log)
+log.setLevel(logging.DEBUG)
+
 # API-ключ созданный ранее
-token = "e0396ed76281c728a25841bf58f1049768b227e85cdaa7f51a073f79ddfcc1f8832e65acc800aaba20a0d"
+token = TOKEN
 
 # Авторизуемся как сообщество
 vk = vk_api.VkApi(token=token)
@@ -119,7 +143,7 @@ longpoll = VkLongPoll(vk)
 # Список подключенных ботов
 bot_online = {}
 
-print("Бот запущен")
+log.debug("Бот активен")
 
 # Основной цикл
 for event in longpoll.listen():
@@ -131,19 +155,26 @@ for event in longpoll.listen():
         if event.to_me:
 
             # Сообщение от пользователя
+            log.info(f"Пришло сообщение от {event.user_id} с содержанием: {event.text}")
             request = event.text.lower()
 
             # Имя пользователя
             user = str(event.user_id)
 
             if user in bot_online and bot_online[user] is not None:
-                print(f"Объект {user} есть в списке, продолжаем работу")
+                log.debug(f"Объект {user} есть в списке")
                 bot_online[user].act(request)
-                for item in bot_online:
-                    if bot_online[item].complete_application:
-                        bot_online[item] = None
+                if len(bot_online) != 0:
+                    for item in bot_online:
+                        if bot_online[item].complete_application:
+                            log.debug(f"Объект {bot_online[item]} удален из списка")
+                            bot_online[item] = None
+                            bot_online.pop(item)
+                        if len(bot_online) == 0:
+                            break
+
             else:
-                print(f"Объекта {user} нет в списке")
+                log.debug(f"Объекта {user} нет в списке")
                 bot_online[user] = Vk_bot(event.user_id)
                 bot_online[user].act(request)
-                print("Словарь действующих ботов:", bot_online)
+                log.debug(f"Словарь действующих ботов: {bot_online}")
